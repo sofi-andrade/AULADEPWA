587dba34592afb67f43a32911de0f5652275f0cb
sofi-andrade <sofiaisabelleandraderocha@gmail.com>
2026-05-25 22:16:23 -0300

    Add files via upload

---
+++ AULADEPWA-main/AULADEPWA-main/sw.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+        .then(function(registration) {
+            console.log('Service Worker registrado com sucesso:', registration);
+        })
+        .catch(function(error) {
+            console.log('Falha ao registrar o Service Worker:', error);
+        });     
+} else {
+    console.log('Service Workers não são suportados neste navegador.');
+}
